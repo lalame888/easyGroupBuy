0f0400f1f9fbf71322eb9c649561c6c12d5746ed
黃韻儒 <lalame888@gmail.com>
2022-02-07 11:42:28 +0800

    專案起始

---
+++ apps/easy-group-buy/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'easy-group-buy',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/easy-group-buy',
+};
